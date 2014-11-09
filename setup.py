31833bab2115ed8d2389f6bb8cb70e773c88c7a5
Alpha <alpha@pokesplash.net>
2014-11-08 21:53:05 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pressure-layout',
+      version='0.1',
+      description='Automatic Pressure Generator',
+      url='http://github.com/Alphadelta14/python-pressure-layout',
+      author='Alpha',
+      author_email='alpha@projectpokemon.org',
+      license='MIT',
+      packages=['pressure'],
+      zip_safe=False)
